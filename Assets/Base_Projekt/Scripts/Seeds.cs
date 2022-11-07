463c876286c2aed8c1e008d433f4aaf2fe8284db
Julie-Haupt <julielucy11@gmail.com>
2022-11-07 09:58:02 +0100

    added seed function
    Added seeds fucntion for planting.

---
+++ Assets/Base_Projekt/Scripts/Seeds.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Seeds : MonoBehaviour
+{
+    public TMP_Text seedText;
+    public int seed = 0;
+    public GameObject FirstBox;
+    public GameObject SecondBox;
+    public GameObject ThirdBox;
+    public GameObject FourthBox;
+    public GameObject FifthBox;
+
+    void Update()
+    {
+        seedText.text = "Samenanzahl: " + seed.ToString();
+
+        if (seed < 1)
+        {
+            FirstBox.SetActive(false);
+            SecondBox.SetActive(false);
+            ThirdBox.SetActive(false);
+            FourthBox.SetActive(false);
+            FifthBox.SetActive(false);
+        }
+        else
+        {
+            FirstBox.SetActive(true);
+            SecondBox.SetActive(true);
+            ThirdBox.SetActive(true);
+            FourthBox.SetActive(true);
+            FifthBox.SetActive(true);
+        }
+    }
+
+    public void GetSeed()
+    {
+        seed = seed + 1;
+    }
+
+    public void Plant()
+    {
+        seed = seed - 1;
+    }
+}
